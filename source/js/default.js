3145823db80ce35469b4691836fd33699984d578
GurovDmitriy <gurovdmitriy1991@gmail.com>
2020-08-18 18:24:52 +0300

    Added base bs grid class

--- source/js/default.js
+++ source/js/default.js
@@ -1,2 +1,12 @@
 'use strict'
 
+$(window).scroll(function() {
+  $(".slideanim").each(function(){
+    var pos = $(this).offset().top;
+
+    var winTop = $(window).scrollTop();
+    if (pos < winTop + 600) {
+      $(this).addClass("slide");
+    }
+  });
+});
